6cd8b859543153c477e90168005b40381cd70e01
Jeff Perkinson <jmperkinson@gmail.com>
2020-06-11 14:30:38 -0400

    Change h5 font to white for individual pet page.

--- app/Support/HelperClass.php
+++ app/Support/HelperClass.php
@@ -79,7 +79,7 @@ class HelperClass
                 <div class="row">
                     <div class="col-sm-2"></div>
                     <div class="col-sm-8"> 
-                        <h4>' . $pet["pet_description"] . '</h4>
+                        <h5>' . $pet["pet_description"] . '</h5>
                     </div>
                     <div class="col-sm-2"></div>
                 </div>
